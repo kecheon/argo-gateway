7168475d7b084d57f2d3259b00c215372ec100fb
Alan Anderson <alanpurple@gmail.com>
2020-12-03 18:14:41 +0900

    revert false email validation

--- ClientApp/src/app/login.ts
+++ ClientApp/src/app/login.ts
@@ -1,13 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './login.html',
   styleUrls: ['./login.sass']
 })
 export class Login implements OnInit {
-
-  email = new FormControl('', [Validators.required, Validators.email]);
 
   username: string = '';
   password: string = '';
@@ -19,13 +16,5 @@ export class Login implements OnInit {
   resetId(): void {
     this.username = '';
     this.password = '';
-  }
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'Username required.';
-    }
-    else
-      return this.email.hasError('email') ? 'Not a valid E-mail' : '';
   }
 }
