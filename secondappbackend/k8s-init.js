310f9707b465df2c76c96ede1e287e63726e837f
Alan Anderson <alanpurple@gmail.com>
2020-12-08 17:23:27 +0900

    add k8s connection

---
+++ secondappbackend/k8s-init.js
@@ -0,0 +1,28 @@
+const k8s = require('@kubernetes/client-node');
+const tempdb_session = require('./connect-db');
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+const kc = new k8s.KubeConfig();
+kc.loadFromOptions(require('./kube.config.json'));
+const client = kc.makeApiClient(k8s.CoreV1Api);
+/*module.exports = (async () => {
+    try {
+        const session = await tempdb_session.getSession();
+        const result = await session.sql(`SELECT * FROM tempdb.cluster_infos WHERE name='admin'`).execute();
+        data = result.fetchOne();
+        const k8s_token = 'Bearer ' + data[5];
+        client.defaultHeaders = {
+            Authorization: k8s_token
+        };
+        return client;
+    }
+    catch (err) {
+        console.error(err);
+    }
+});*/
+client.defaultHeaders = {
+    Authorization: 'Bearer ' + require('./ksinfo.json').K8S_TOKEN
+};
+
+module.exports = client;
